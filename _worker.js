98dd38b7ec8b76cef6af3830f8d7b9e4b5f4e436
gcl <gchangliu@gmail.com>
2024-11-17 10:17:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '10cd5940-a5b5-4bf1-bf19-a6a9bc762ec6';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
